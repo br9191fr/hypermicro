ed9c34cb875a6a5131f99d64cc587230f745401c
Bruno Ricci <br9191fr@gmail.com>
2022-04-02 18:43:12 +0200

    Implement POST and GET

--- src/bin/warp2.rs
+++ src/bin/warp2.rs
@@ -12,6 +12,28 @@ use httplib::types::{ItemsDb};
 async fn main() {
     let items_db: ItemsDb = Arc::new(Mutex::new(HashMap::new()));
     let root = warp::path::end().map(|| "Welcome to my warp server!");
-    let routes = root.with(warp::cors().allow_any_origin());
-    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+    let shopping_list_items_route = warp::path("shopping_list_items")
+        .and(warp::get())
+        .and(with_items_db(items_db.clone()))
+        .and_then(handlers::get_shopping_list_items);
+    let shopping_list_item_route = warp::path("shopping_list_item")
+        .and(warp::post())
+        .and(warp::body::json())
+        .and(with_items_db(items_db.clone()))
+        .and_then(handlers::create_shopping_list_item)
+        .or(warp::path!("shopping_list_item" / usize)
+            .and(warp::get())
+            .and(with_items_db(items_db.clone()))
+            .and_then(handlers::get_shopping_list_item_by_id));
+
+    let routes = root
+        .or(shopping_list_items_route)
+        .or(shopping_list_item_route)
+        .with(warp::cors().allow_any_origin());
+    warp::serve(routes).run(([127, 0, 0, 1], 5000)).await;
+}
+fn with_items_db(
+    items_db: ItemsDb,
+) -> impl Filter<Extract = (ItemsDb,), Error = Infallible> + Clone {
+    warp::any().map(move || items_db.clone())
 }
--- src/handlers.rs
+++ src/handlers.rs
@@ -8,3 +8,35 @@ pub async fn get_shopping_list_items(items_db: ItemsDb) -> Result<impl Reply> {
     let local_db: Vec<models::ShoppingListItem> = local_db.values().cloned().collect();
     Ok(reply::with_status(reply::json(&local_db), StatusCode::OK))
 }
+
+pub async fn create_shopping_list_item(
+    mut shopping_list_item: models::ShoppingListItem,
+    items_db: ItemsDb,
+) -> Result<impl Reply> {
+    println!("Received UserData: {:?}", shopping_list_item);
+    let mut local_db = items_db.lock().await;
+    let key_count = local_db.keys().len();
+    shopping_list_item.item_id = Some(key_count);
+    local_db.insert(key_count, shopping_list_item.clone());
+    Ok(reply::with_status(
+        reply::json(&shopping_list_item),
+        StatusCode::CREATED,
+    ))
+}
+
+pub async fn get_shopping_list_item_by_id(id: usize, items_db: ItemsDb) -> Result<impl Reply> {
+    let local_db = items_db.lock().await;
+    let shopping_list_item = match local_db.get(&id) {
+        Some(item) => item,
+        _ => {
+            return Ok(reply::with_status(
+                reply::json(&"{}"),
+                StatusCode::NOT_FOUND,
+            ));
+        }
+    };
+    Ok(reply::with_status(
+        reply::json(&shopping_list_item),
+        StatusCode::OK,
+    ))
+}
